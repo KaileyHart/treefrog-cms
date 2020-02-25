56fe9feb41ccc8974688620dc775ca92d61669e6
Kailey <kalyhart@iu.edu>
2020-02-25 08:09:09 -0500

    Created New Page & Modal

--- public/app/app.js
+++ public/app/app.js
@@ -1,21 +1,141 @@
 function initButtons() {
-  $(".get-started").click(function(e) {
-    $("#home div").removeClass("active");
-    $("#addNav div").addClass("active");
-
-    $(".text-wrapper").html(TREEFROG_SERVICE.getGetStartedContent());
-    $(".btn-holder").html(TREEFROG_SERVICE.getCreateNavButton());
-  });
-
+  //Makes it so that when the user clicks on the HOME button they can go back to home from the add nav bar in the top navigation
   $("#home").click(function() {
     $("#addNav div").removeClass("active");
     $("#home div").addClass("active");
 
     $(".text-wrapper").html(TREEFROG_SERVICE.getHomeContent());
     $(".btn-holder").html(TREEFROG_SERVICE.getHomeStartButton());
+    addGetStartedListener();
+    //turns off the listener on the create main nav button
+    $("#createMainNav").off();
+  });
+
+  //Makes it so that when the user clicks the Create Main Nav button, it opens the modal
+  $(document).on("click", "#create-nav", function() {
+    $(".modal, .alert-box").addClass("active");
+
+    $(".btn-holder").html(TREEFROG_SERVICE.getCreateModalPopup());
+  });
+
+  //Grabs each spot where the ".closeModal" class is put and it runs the closeModal Function
+  $(".closeModal").click(function() {
+    closeModal();
+  });
+}
+
+$(document).on("click", "#submit-nav-input", function() {
+  //Removes the modal
+  $(".modal, .alert-box").removeClass("active");
+
+  //adds the Text editor Content
+  $(".text-wrapper").html(TREEFROG_SERVICE.getTextEditorContent());
+  $(".editor").html(TREEFROG_SERVICE.getQuillContainer());
+  createQuillContainer();
+});
+
+//Console.log's what the user puts in the input in the modal
+//Turns the input value into ALL lowercase
+//Alerts the user if the input is empty in the modal
+$("#submit-nav-input").click(function() {
+  //Alerts the user if the input is empty in the modal
+  if ($("#nav-input").val() == "") {
+    alert("Input is empty! Please add a navigation name to continue");
+    $(".modal, .alert-box").addClass("active");
+    $(".btn-holder").html(TREEFROG_SERVICE.getCreateModalPopup());
+  } else if ($("nav-input").val() != "") {
+    alert(
+      $(".nav-input")
+        .val()
+        .toLowerCase() + " has been created!"
+    );
+  }
+
+  //Console.log's what the user puts in the input in the modal
+  console.log($("#nav-input").val());
+
+  //Turns the input value into ALL lowercase
+  console.log(
+    $("#nav-input")
+      .val()
+      .toLowerCase()
+  );
+
+  if ($("#nav-input").val() == $("nav-input").val()) {
+    alert("You already made this navigation! Please make a new one!");
+  } else if ($("#nav-input").val() != $("nav-input").val()) {
+    $("#nav-input-submit").click(function() {
+      $(".modal").removeClass("active");
+      $(".create-nav").addClass("active");
+
+      $(".text-wrapper").html(TREEFROG_SERVICE.getHomeContent());
+      $(".btn-holder").html(TREEFROG_SERVICE.getHomeStartButton());
+
+      addGetStartedListener();
+      //turns off the listener on the create main nav button
+      $("#createMainNav").off();
+    });
+  }
+});
+
+function createQuillContainer() {
+  var container = $(".editor").get(0);
+  var editor = new Quill(container);
+
+  var quill = new Quill("#editor-container", {
+    modules: {
+      toolbar: [
+        ["bold", "italic", "underline", "strike"], //toggled buttons
+        ["blockquote", "code-block"],
+
+        [{ header: 1 }, { header: 2 }], //custom button values
+        [{ list: "ordered" }, { list: "bullet" }],
+        [{ script: "sub" }, { script: "super" }][ //sperscript/subscript
+          ({ indent: "-1" }, { indent: "+1" })
+        ], //outdent/indent
+        [{ direction: "rtl" }], //text-direction
+
+        [{ size: ["small", false, "large", "huge"] }], //custom dropdown
+        [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+        [{ color: [] }, { background: [] }][{ font: [] }], //dropdown defaults from theme
+        [{ align: [] }],
+
+        ["clean"] //remove formatting button
+      ]
+    },
+    placeholder: "Compose an epic...",
+    theme: "snow" // or "bubble"
+  });
+}
+
+//Closes the modal
+function closeModal() {
+  $(".modal").css("display", "none");
+}
+
+//Turns off the create main nav button when you click cancel on the modal
+function addCreateMNListener() {
+  $("#createMainNav").click(function(e) {
+    $(".modal").css("display", "flex");
+  });
+}
+
+//Turns off the listener on the get started button
+function addGetStartedListener() {
+  $(".get-started").click(function(e) {
+    $("#home div").removeClass("active");
+    $("#addNav div").addClass("active");
+
+    $(".text-wrapper").html(TREEFROG_SERVICE.getGetStartedContent());
+    $(".btn-holder").html(TREEFROG_SERVICE.getCreateNavButton());
+    addCreateMNListener();
+    $(".get-started").off("click");
   });
 }
 
 $(document).ready(function() {
   initButtons();
+  addGetStartedListener();
+  createQuillContainer();
 });
--- public/app/service.js
+++ public/app/service.js
@@ -1,16 +1,60 @@
 var TREEFROG_SERVICE = (function() {
+  var _getQuillContainer = function() {
+    let quillContent = ``;
+
+    return quillContent;
+  };
+
+  //Stores the text editor Content
+  var _getTextEditorContent = function() {
+    let textEditorContent = `<h1>Treefrog CMS</h1><p>Now you have your navigation set now you can create your content. Create your content in the text editor and then click on "Save Page Info". Once you have done that, click on "PREVIEW SITE" to see what your webage looks like.</p><div id="editor">
+    <p>Hello World!</p>
+    <p>Some initial <strong>bold</strong> text</p> 
+    <p><br></p>
+    </div>
+    
+    `;
+
+    return textEditorContent;
+  };
+
+  //Stores the buttons within the Modal popup content
+  var _getModalPopupButtons = function() {
+    let modalPopupButtons = `<div class="btn-holder-modal">
+    <span class="btn btn-light ">Create Main Nav</span
+    ><span id="close-modal" class="btn btn-light">Cancel</span>
+  </div>`;
+
+    return modalPopupButtons;
+  };
+
+  //Stores the Modal popup content
+  var _getCreateModalPopup = function() {
+    let modalPopup = `<div class="modal">
+    <div class="alert-box">
+    <h2>Use this box to create navigation links.</h2>
+    <p>You can create main navigation and sub navigation. To create a sub-navigation you will first need to select a main nav and then create the sub nav.  </p> 
+    <p >Using the text box below enter the name of your main navigation</p> 
+    <input type="text">`;
+
+    return modalPopup;
+  };
+
+  //Stores the add navigation content
   var _getGetStartedContent = function() {
     let contentString = `<h1>Treefrog CMS</h1><p>This is the screen where you will create your navigation and page content</p><p>First, you will need to create a main navigation. Once you have created a main navigation you can create a sub-navigation if you would like to. </p><p>Once you create either a nav or sub-nav a text editor will pop up and you will be allowed to create your page content.</p>`;
 
     return contentString;
   };
 
+  //Stores the Create Main Nav and Create Sub Nav buttons
   var _getCreateNavButton = function() {
-    let buttonString = `<span class="btn btn-dark">Create Main Nav</span><span class="btn btn-dark">Create Sub Nav</span>`;
+    let buttonString = `<span   id="create-nav" id="createMainNav" class="btn btn-dark">Create Main Nav</span><span class="btn btn-dark">Create Sub Nav</span>`;
 
     return buttonString;
   };
 
+  //Stores the homepage content
   var _getHomeContent = function() {
     let homeContent = `<h1>Welcome to the Treefrog CMS</h1>
     <p>
@@ -33,6 +77,7 @@ var TREEFROG_SERVICE = (function() {
     return homeContent;
   };
 
+  //Stores the Get started button
   var _getHomeStartButton = function() {
     let startButton = `<div class="btn-holder">
     <span class="btn btn-dark get-started">Get Started</span>
@@ -45,6 +90,10 @@ var TREEFROG_SERVICE = (function() {
     getGetStartedContent: _getGetStartedContent,
     getCreateNavButton: _getCreateNavButton,
     getHomeContent: _getHomeContent,
-    getHomeStartButton: _getHomeStartButton
+    getHomeStartButton: _getHomeStartButton,
+    getCreateModalPopup: _getCreateModalPopup,
+    getModalPopupButtons: _getModalPopupButtons,
+    getTextEditorContent: _getTextEditorContent,
+    getQuillContainer: _getQuillContainer
   };
 })();
